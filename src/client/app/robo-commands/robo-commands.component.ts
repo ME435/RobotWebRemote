a8bb85f50c297fcce30a02232012ed1fb740e2ca
Matt Boutell <boutell@gmail.com>
2016-05-02 11:43:16 -0400

    Sends freeze command on button click

--- src/client/app/robo-commands/robo-commands.component.ts
+++ src/client/app/robo-commands/robo-commands.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from 'angular2/core';
+import {Component, OnInit, Inject} from 'angular2/core';
 import {MD_CARD_DIRECTIVES} from "@angular2-material/card";
 import {MdButton} from "@angular2-material/button";
+import {AngularFire} from "angularfire2/angularfire2";
+import {FirebaseRef} from "angularfire2/tokens";
 
 @Component({
   selector: 'robo-commands',
@@ -10,8 +12,13 @@ import {MdButton} from "@angular2-material/button";
 })
 export class RoboCommandsComponent implements OnInit {
 
-  constructor() {}
-  
+  commandsRef: Firebase;
+
+  constructor(@Inject(FirebaseRef) ref: Firebase) {
+    // TODO: DOn't hardcode robot name
+    this.commandsRef = ref.child("elmo/commands");
+
+  }
   ngOnInit() {
   }
 
