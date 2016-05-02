accdf9d6ea2e9c4d63a591d8da0f383c611d8564
Matt Boutell <boutell@gmail.com>
2016-05-02 10:05:00 -0400

    Remove dist

--- dist/vendor/angularfire2/providers/web_workers/shared/channels.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.AUTH_CHANNEL = 'angularfire2-auth';
-exports.INITIAL_AUTH_CHANNEL = 'angularfire2-auth-initial';
-//# sourceMappingURL=channels.js.map
--- dist/vendor/angularfire2/utils/absolute_path_resolver.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function absolutePathResolver(rootUrl, providedPath) {
-    return providedPath.indexOf('/') === 0 ? "" + rootUrl + providedPath : providedPath;
-}
-exports.absolutePathResolver = absolutePathResolver;
-//# sourceMappingURL=absolute_path_resolver.js.map
