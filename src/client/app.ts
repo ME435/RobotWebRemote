c5fc12e4b4943e379891b74210071540116e8e08
Matt Boutell <boutell@gmail.com>
2016-05-02 09:37:11 -0400

    7: Bootstrap

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {RobotWebRemoteApp} from './app/robot-web-remote';
+import {ROUTER_PROVIDERS} from "angular2/router";
+import {FIREBASE_PROVIDERS, defaultFirebase} from "angularfire2/angularfire2";
 
-bootstrap(RobotWebRemoteApp, []);
+bootstrap(RobotWebRemoteApp, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase("https://fisherds-robotwebremote.firebaseio.com"),
+  ROUTER_PROVIDERS,
+]);
