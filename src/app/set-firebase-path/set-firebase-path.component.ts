4aa4a9e020db0da311ce472ec456025af819c900
David Fisher <fisherds@rose-hulman.edu>
2016-05-12 00:19:00 -0400

    Made an event emitter for the firebase path to nowhere
    Made a pretty EventEmitter, but where does it go when the component is
    loaded by the router. :)  Nowhere.  I need to move to use a
    FirebaseState service for sure.

--- src/app/set-firebase-path/set-firebase-path.component.ts
+++ src/app/set-firebase-path/set-firebase-path.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import {MdInput} from "@angular2-material/input/input";
+import {MdButton} from "@angular2-material/button/button";
 
 @Component({
   moduleId: module.id,
   selector: 'app-set-firebase-path',
   templateUrl: 'set-firebase-path.component.html',
-  styleUrls: ['set-firebase-path.component.css']
+  styleUrls: ['set-firebase-path.component.css'],
+  directives: [MdInput, MdButton],
+  outputs: ["setFirebaseUrl"],
 })
 export class SetFirebasePathComponent implements OnInit {
 
-  constructor() {}
+  private firebaseUrl = "fisherds-robotwebremote";
+  private robotName = "elmo";
+  private setFirebasePath : EventEmitter<string>;
+
+  constructor() {
+    this.setFirebasePath = new EventEmitter();
+  }
 
   ngOnInit() {
+    // TODO: Use localstorage values if present.
   }
+
+  handleSet() {
+    let path = "https://" + this.firebaseUrl + ".firebaseio.com/" + this.robotName;
+    this.setFirebasePath.emit(path);
+    console.log("Just did an emit of " + path);
+  }
+
 
 }
