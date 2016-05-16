f5405f8da1a0f11280b1263b287e8eafa185c7a5
Matt Boutell <boutell@gmail.com>
2016-05-16 10:48:30 -0400

    Custom output emitter name

--- src/app/robot-sidenav-content/robot-sidenav-content.component.ts
+++ src/app/robot-sidenav-content/robot-sidenav-content.component.ts
@@ -8,14 +8,14 @@ import {Output} from "@angular/core/src/metadata";
   templateUrl: 'robot-sidenav-content.component.html',
   styleUrls: ['robot-sidenav-content.component.css'],
   directives: [MdButton],
-  outputs: ["sidenavSelect"],
+  outputs: ["selectionEmitter:sidenavSelect"],
 })
 export class RobotSidenavContentComponent {
 
-  private sidenavSelect: EventEmitter<string> = new EventEmitter();
+  private selectionEmitter: EventEmitter<string> = new EventEmitter();
 
   navigateTo(componentName: string): void {
-    this.sidenavSelect.emit(componentName);
+    this.selectionEmitter.emit(componentName);
   }
 }
 
